5feb608400803ea1bf887d5110d0fa21e6eee9eb
Jordan Godwin <jordan.godwin13@gmail.com>
2023-05-20 20:32:41 -0500

    Add function call to create movie table and print table from master list.

--- mpdb.py
+++ mpdb.py
@@ -1,5 +1,16 @@
 import sqlite3
 
+# establish connection to tutorial.db
+# create tutorial.db if it does not exist
 con = sqlite3.connect('tutorial.db')
 
+#create cursor for db selection
 cur = con.cursor()
+
+# create table 'Movies' in tutorial db with execute method
+# cur.execute('CREATE TABLE movie(title, year, score)')
+
+# query db to ensure movie table exists
+res = cur.execute('SELECT name FROM sqlite_master')
+print(res.fetchone())
+
